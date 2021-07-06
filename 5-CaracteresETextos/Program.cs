d70bb8868415c0ab34ab5656ef43a382175ce301
SLDiego <sldiegoti@gmail.com>
2021-07-06 18:41:09 -0300

    Atributos de Variaveis

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e Textos");
+
+            //Caracteres
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+
+            //String 
+            string titulo = @"Teste de String
+                - linha 1
+                - linha 2";
+
+            Console.WriteLine(titulo);
+
+
+
+
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 6-AtributosDeVariaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtributosDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 30;
+            int idadeDiego = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeDiego);
+
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar");
+            Console.ReadLine();
+
+
+        }
+    }
+}
