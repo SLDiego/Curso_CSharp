790d329beaa9647f678fcb8ec1bbb500fbb56adb
SLDiego <sldiegoti@gmail.com>
2021-07-06 13:02:07 -0300

    Pontos Flutuantes e Conversoes

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1200.70;
+
+            Console.WriteLine(salario);
+            
+
+            double a;
+            a = 15.0 / 2;
+            Console.WriteLine(a);
+
+
+
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair...");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            // O long é uma variavel de 64 bits
+            long a;
+            a = 13000000000;
+            Console.WriteLine("long " + a);
+
+            // O int é uma variavel de 64 bits
+            int b;
+            b = 1300000000;
+            Console.WriteLine("int " + b);
+
+            // O short é uma variavel de 16 bits
+            short quantidaDeProdutos;
+            quantidaDeProdutos = 15000;
+            Console.WriteLine("short " + quantidaDeProdutos);
+
+            // Variaveis do tipo float no C# precisam colocar o "f" ao lado do seu valor
+            float c;
+            c = 1.80f;
+            Console.WriteLine("float " + c);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+
+        }
+    }
+}
