0018425f7f3182b3202c92816f8d601f56be90a6
SLDiego <sldiegoti@gmail.com>
2021-07-07 12:09:34 -0300

    While e For

---
+++ P11 - CalculaPoupança/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11___CalculaPoupança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto P11 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+            
+
+            for (int i = 1; i <= 12; i++)
+            {
+                // 0.36% = 0.0036
+                valorInvestido = valorInvestido * 1.0036;
+                if (i < 2)
+                {                    
+                    Console.WriteLine("Após " + i + " mês você terá o valor " + valorInvestido);
+                }
+                else
+                {
+                    Console.WriteLine("Após " + i + " mêses você terá o valor " + valorInvestido);
+                }
+
+            }
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Tecle enter para encerrar");
+            Console.ReadLine();
+
+        }
+    }
+}
