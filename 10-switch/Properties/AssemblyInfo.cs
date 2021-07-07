78244dddd370c5d3a416a58d7756ecb1a0e2b6e5
SLDiego <sldiegoti@gmail.com>
2021-07-06 22:57:57 -0300

    Condicionais, escopo, switch

---
+++ 10-switch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("10-switch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10-switch")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d0836b96-6bc2-4f0a-941d-114ce58d4d54")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto 8 - Condicionais2");
+
+            int idadeJoão = 18;
+            int quantidadepessoas = 2;
+
+            //bool guarda um valor verdadeiro ou falso
+            bool acompanhado = quantidadepessoas >= 2;
+
+            if (idadeJoão >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar");
+            }
+            else
+            {
+                
+                Console.WriteLine("Não pode entrar.");
+                
+            }
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
