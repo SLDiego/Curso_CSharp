472767c27bd612fd4dcbd7be1a9d45f2dcd7fcf2
SLDiego <sldiegoti@gmail.com>
2021-07-08 16:31:35 -0300

    Funções e Métodos

---
+++ ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+            
+            //Acessando a função SACAR -> Colocar valor entre parenteses
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine("Saldo: " + contaDoBruno.saldo);
+            Console.WriteLine("Retorno da função -> " + resultadoSaque);
+
+            contaDoBruno.Depositar(1500);
+
+            Console.WriteLine("Saldo do Conta " + contaDoBruno.saldo);
+
+
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+            contaDoGabriel.titular = "Gabriel";
+
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriel: " + contaDoGabriel.saldo);
+
+            contaDoBruno.Transferir(100, contaDoGabriel);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriel: " + contaDoGabriel.saldo);
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
