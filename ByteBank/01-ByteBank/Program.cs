ce58accec3aec56a595dcb0605340dcd365bc3e1
SLDiego <sldiegoti@gmail.com>
2021-07-07 23:29:29 -0300

    Objeto, referencia

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+
+            contaDoGabriel.titular = "Gabriel";
+            contaDoGabriel.agencia = 863;
+            contaDoGabriel.numero = 863452;
+            contaDoGabriel.saldo = 100;
+
+            Console.WriteLine("Titular " + contaDoGabriel.titular);
+            Console.WriteLine("Agencia " + contaDoGabriel.agencia);
+            Console.WriteLine("Numero " + contaDoGabriel.numero);
+            Console.WriteLine("Saldo " + contaDoGabriel.saldo);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+            contaDoGabriel.titular = "Gabriel";
+            contaDoGabriel.agencia = 863;
+            contaDoGabriel.numero = 863452;
+
+            ContaCorrente contaDoGabriel2 = new ContaCorrente();
+            contaDoGabriel2.titular = "Gabriel";
+            contaDoGabriel2.agencia = 863;
+            contaDoGabriel2.numero = 863452;
+
+            //Referencia a ponta para um Objeto na memoria do PC
+            Console.WriteLine("Igualdade do tipo de referencia " + (contaDoGabriel == contaDoGabriel2));
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
