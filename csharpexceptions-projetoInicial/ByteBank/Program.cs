3066bf8844551a1ee07fa95747c1f5b114661f46
SLDiego <sldiegoti@gmail.com>
2021-07-15 16:21:10 -0300

    excessoes

--- csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -10,11 +10,53 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            /*
             ContaCorrente conta = new ContaCorrente(7480, 874150);
 
             Console.WriteLine(ContaCorrente.TaxaOperacao);
+            */
+            try
+            {
+                Metodo();
 
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+                
+            }
+            
             Console.ReadLine();
         }
+
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+        static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+
+
+
     }
+
+
 }
